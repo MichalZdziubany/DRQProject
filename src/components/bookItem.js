81f75a9250f13aa695903ff73a23b93cbe63de52
MichalZdziubany <127780743+MichalZdziubany@users.noreply.github.com>
2024-12-11 14:22:53 +0000

    added deletion functinality in my book item component

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
 import Card from 'react-bootstrap/Card';
-
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Button } from "react-bootstrap";
 
 const BookItem = (props)=> {
   useEffect(() => {
     console.log("Movie Item:", props.mybook);
   }, [props.mybook]); // Only run this effect when the mybook prop changes
 
+  const handleDelete = (e)=>{
+    e.preventDefault();
 
+    axios.delete('http://localhost:4000/api/book/'+props.mybook._id)
+    .then((res)=>{
+      props.Reload();
+    })
+    .catch((error)=>{
+      console.log(error);
+    });
+
+  }
 
   return (
     <div>
@@ -20,6 +33,9 @@ const BookItem = (props)=> {
             <footer>{props.mybook.author}</footer>
           </blockquote>
         </Card.Body>
+        <Link to={"/edit/" + props.mybook._id} className="btn btn-primary">Edit Book</Link>
+        {/* delete button to invoke handle delete method */}
+        <Button variant="danger" onClick={handleDelete}>Delete</Button>
       </Card>
     </div>
   );
